201be05bfd61ccde078698ff68b1dd689e75631d
unknown <jesusreyesreal@gmail.com>
2019-06-18 00:03:11 +0200

    asdf

--- server/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const URI = 'mongodb://localhost/musicstats'; 
-
-mongoose.connect(URI)
-    .then(db => console.log('db conected'))
-    .catch(err => console.log(err));
-
-module.exports.mongoose;
--- server/index.js
+++ server/index.js
@@ -1,23 +1,37 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
+var request = require('request');
+var express = require('express');
+var app = express();
 
-const app = express();
-
-const {mongoose} = require ('./database')
-
-//settings
 app.set('port', process.env.PORT || 3000);
 
-//middlewares
-app.use(morgan('dev'));
-app.use(express.json ());
-app.use(cors({origin: 'http://localhost:4200'}));
+app.get('/token', function(req, resp) {
+  resp.header('Access-Control-Allow-Origin', '*');
+  resp.header('Access-Control-Allow-Headers', 'X-Requested-With');
 
-//Routes
-app.use('/api/users', require ('./rutas/user.route'));
+  var client_id = 'd3c6cfa676cb4f108f5fa16074fb82d4';
+  var client_secret = '448b55948e8040c6bc9421e8d6428ca6';
 
+  // your application requests authorization
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      Authorization:
+        'Basic ' +
+        new Buffer(client_id + ':' + client_secret).toString('base64')
+    },
+    form: {
+      grant_type: 'client_credentials'
+    },
+    json: true
+  };
 
-app.listen(app.get('port'), ()=>{
-    console.log('server on port', app.get('port'));
-})
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      resp.json({ token: body.access_token });
+    }
+  });
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
